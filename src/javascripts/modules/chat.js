eab0f1b3163b0235129fd3040624c0b87a1e0d81
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2019-08-17 18:50:44 +0900

    add modules

---
+++ src/javascripts/modules/chat.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const URL1 = 'https://script.google.com/macros/s/AKfycbxBjok29e-9FOoW-sAAIw9ZV2Sh8vc6-L0nY6oEn5uEdXKenwZk/exec';
+
+export default class Chat{
+    constructor(){
+    }
+    async send(name, comment){
+    	return await axios.post(URL1,{
+    		name: name,
+    		comment: comment
+    	}, {
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        })
+        .catch(err => err);
+    }
+
+    async fetch(){
+    	return await axios.get(URL1).catch(err => err);
+    }
+}
